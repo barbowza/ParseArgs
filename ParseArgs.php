1bde506a9f53b489fb4cf57afe7b64d38bdd1007
barbowza <barbowza@users.noreply.github.com>
2022-08-25 15:10:01 +0100

    Place class in namespace

--- ParseArgs.php
+++ ParseArgs.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+namespace Barbowza;
+
 /**
  * Command Line Arguments parsing utility class.
  *
--- tests/ParseArgsTest.php
+++ tests/ParseArgsTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use Barbowza\ParseArgs;
 
 require __DIR__ . '/../vendor/autoload.php';
 
